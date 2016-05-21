ce55f8da8c131703eeb44ab16984a54f331e13fb
Mariz <gmlunesa@gmail.com>
2016-05-21 11:57:38 +0800

    Add files via upload

--- database/supervisionv1.sql
+++ database/supervisionv1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2016 at 05:48 PM
+-- Generation Time: May 21, 2016 at 05:56 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.5.34
 
@@ -41,7 +41,9 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`Cust_ID`, `Cust_FName`, `Cust_LName`, `Cust_Num`, `Cust_Email`) VALUES
 (18, 'Goldy', 'Lunesa', 9988949125, 'gmlunesa@gmail.com'),
 (19, 'Daenerys', 'Targaryen', 89989, 'dany@gmail.com'),
-(20, 'John', 'Doe', 654321, 'jd@gmail.com');
+(20, 'John', 'Doe', 654321, 'jd@gmail.com'),
+(21, 'Barack', 'Obama', 911, 'obama@gmail.com'),
+(22, 'Dave', 'Starfield', 7889888, 'dave@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -74,10 +76,12 @@ CREATE TABLE `products` (
 INSERT INTO `products` (`Prod_ID`, `Prod_Name`, `Prod_Desc`, `Prod_Design`, `Design_Price`, `Pickup_Date`, `Width`, `Height`, `Shape`, `Size`, `Color`, `Type`, `Quantity`, `Total_Price`, `Cust_ID`) VALUES
 (11, 'Bag Tag', '-', '#20', 80, '2016-05-21', NULL, NULL, NULL, NULL, NULL, 'Rubber', 5, 400, 18),
 (12, 'Bag Tag', 'Gold', '#2', 50, '2016-05-23', NULL, NULL, NULL, NULL, NULL, 'Clear Plastic', 3, 150, 18),
-(13, 'Bag Tag', 'Round', '#31', 60, '2016-05-17', NULL, NULL, NULL, NULL, NULL, 'Rubber', 4, 240, 18),
+(13, 'Bag Tag', 'Yes, the design is this', '#31', 60, '2016-05-17', NULL, NULL, NULL, NULL, NULL, 'Rubber', 4, 240, 18),
 (14, 'Bag Tag', 'Thick', '#5', 80, '2016-05-17', NULL, NULL, NULL, NULL, NULL, 'Rubber', 1, 80, 19),
 (15, 'Bag Tag', 'Cloudy', '#34', 70, '2016-05-17', NULL, NULL, NULL, NULL, NULL, 'Glass', 3, 210, 19),
-(17, 'Bag Tag', 'Painted', '#4', 30, '2016-05-21', NULL, NULL, NULL, NULL, NULL, 'Glass', 4, 120, 20);
+(17, 'Bag Tag', 'Painted', '#4', 30, '2016-05-21', NULL, NULL, NULL, NULL, NULL, 'Glass', 4, 120, 20),
+(19, 'Bag Tag', 'Smooth', '#80', 50, '2016-05-22', NULL, NULL, NULL, NULL, NULL, 'Glass', 5, 250, 21),
+(20, 'Bag Tag', 'Something', '#23', 80, '2016-05-18', NULL, NULL, NULL, NULL, NULL, 'Glass', 5, 400, 22);
 
 --
 -- Indexes for dumped tables
@@ -106,12 +110,12 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `Cust_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `Cust_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `Prod_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `Prod_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- Constraints for dumped tables
 --
